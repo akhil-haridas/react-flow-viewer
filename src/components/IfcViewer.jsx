1eb84de7599b11f65c77137a7de173918f065d47
akhil-haridas <4khilharidas@gmail.com>
2024-08-07 15:16:22 +0530

    comp for the ifc viewer

---
+++ src/components/IfcViewer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const IfcViewer = () => {
+    return (
+        <div
+            style={{
+                width: "400px",
+                height: "300px",
+                background: 'gray'
+            }}
+        >
+            <div className="ViewerTitleWrapper">
+                <span>Ifc viewer</span>
+            </div>
+        </div>
+    );
+};
+
+export default IfcViewer;
