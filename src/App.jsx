e280f71e2a80311a41871906beb79b8c39cb03d1
akhil-haridas <4khilharidas@gmail.com>
2024-08-07 15:15:35 +0530

    using Flow wrapper directly into the app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+import { FlowWrapper } from "./components";
+
+function App() {
+  return (
+    <div className="backgroundWrapper">
+      <div className="viewerWrapper">
+        <FlowWrapper />
+      </div>
+    </div>
+  );
+}
+
+export default App;
