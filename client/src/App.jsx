c635c4e92c82efe4a108d6fe35f50b30c1a07c30
akhil-haridas <4khilharidas@gmail.com>
2024-08-23 17:30:06 +0530

    added web audio component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 
 import React from "react";
+import WebAudio from "./components/WebAudio";
 
 // import { FlowWrapper } from "./components";
-
 
 function App() {
   return (
     <div className="backgroundWrapper">
       <div className="viewerWrapper">
         {/* <FlowWrapper /> */}
+        <WebAudio />
       </div>
     </div>
   );
