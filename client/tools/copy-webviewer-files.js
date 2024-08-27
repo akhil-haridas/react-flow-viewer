69182ffc3823c881eb195fe521b8a3aa49ebbf49
akhil-haridas <4khilharidas@gmail.com>
2024-08-27 17:45:52 +0530

    context and utils for the html viewer

--- client/tools/copy-webviewer-files.js
+++ client/tools/copy-webviewer-files.js
@@ -2,8 +2,7 @@ const fs = require('fs-extra');
 
 const copyFiles = async () => {
   try {
-    await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
-    await fs.copy('./node_modules/@pdftron/webviewer-audio/src/lib/api/ServiceWorker/wv-audio-injection.js', './public/wv-audio-injection.js');
+    await fs.copy('./node_modules/@pdftron/webviewer/public', './public/lib');
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
