6f8b2293ddbf4b7f0c1d18ac3f0d3e1a88d0c535
akhil-haridas <4khilharidas@gmail.com>
2024-09-02 13:01:03 +0530

    initial as 3 nodes

--- client/src/components/FlowWrapper.jsx
+++ client/src/components/FlowWrapper.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   ReactFlow,
   MiniMap,
   Background,
   BackgroundVariant,
   Controls,
+  useNodesState,
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
@@ -19,14 +20,48 @@ const initialNodes = [
     id: '1',
     type: 'CustomResizerNode',
     data: { label: 'Custom Resize Icon', viewer: "PdftronViewer" },
-    position: { x: 150, y: 150 },
+    position: { x: 70, y: 166 },
     style: {
       background: '#fff',
       fontSize: 12,
       border: '1px solid black',
       padding: 5,
       borderRadius: 15,
-      height: 100,
+      // height: 100,
+      height: "990px",
+      width: "1460px"
+    },
+  },
+  {
+    id: '2',
+    type: 'CustomResizerNode',
+    data: { label: 'Custom Resize Icon', viewer: "PdftronViewer" },
+    position: { x: -1452, y: 1802 },
+    style: {
+      background: '#fff',
+      fontSize: 12,
+      border: '1px solid black',
+      padding: 5,
+      borderRadius: 15,
+      // height: 100,
+      height: "990px",
+      width: "1460px"
+    },
+  },
+  {
+    id: '3',
+    type: 'CustomResizerNode',
+    data: { label: 'Custom Resize Icon', viewer: "PdftronViewer" },
+    position: { x: -2650, y: 166 },
+    style: {
+      background: '#fff',
+      fontSize: 12,
+      border: '1px solid black',
+      padding: 5,
+      borderRadius: 15,
+      // height: 100,
+      height: "990px",
+      width: "1460px"
     },
   },
 ];
@@ -34,6 +69,11 @@ const initialNodes = [
 const initialEdges = [];
 
 const FlowWrapper = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+
+  useEffect(() => {
+    console.log(nodes)
+  }, [nodes])
   return (
     <div className="flowWrapper">
       {/* <div className='viewerWrapper'> */}
@@ -41,7 +81,8 @@ const FlowWrapper = () => {
         defaultNodes={initialNodes}
         defaultEdges={initialEdges}
         className="react-flow-node-resizer-example"
-        minZoom={0.2}
+        onNodesChange={onNodesChange}
+        // minZoom={-100}
         maxZoom={4}
         fitView
         nodeTypes={nodeTypes}
