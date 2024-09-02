496bbd536716c572c56ef12f20c0665aa5ebf2f2
akhil-haridas <4khilharidas@gmail.com>
2024-09-02 13:01:17 +0530

    pdftron viewer rendering with file

--- client/src/components/Pdftron/PdftronViewer.jsx
+++ client/src/components/Pdftron/PdftronViewer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import WebViewer from '@pdftron/webviewer';
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 const PdftronViewer = () => {
-  return (
-    <div>PdftronViewer</div>
-  )
+    const [instance, setInstance] = useState(null)
+    const viewer = useRef(null);
+
+    const initializeViewer = useCallback(async () => {
+        console.log(instance)
+        if (instance) return
+        console.log('instance')
+
+        if (viewer.current) {
+            try {
+                const webinstance = await WebViewer(
+                    {
+                        path: "/webviewer",
+                        licenseKey: "demo:1721108165987:7f9a5c7b03000000007463b7c7a77a620e9c805eedc37b89008b5843ac",
+                        // initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+                    },
+                    viewer.current
+                );
+                setInstance(webinstance)
+            } catch (error) {
+                console.error("Error initializing WebViewer:", error);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        initializeViewer()
+    }, [initializeViewer]);
+
+    useEffect(() => {
+        if (instance) {
+            instance.UI.loadDocument("https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf");
+        }
+    }, [instance]);
+
+    return (
+        <div className="webviewer" ref={viewer} style={{ height: "100%", width:"100%" }}></div>
+    );
 }
 
 export default PdftronViewer
--- client/src/components/Resizer/CustomResizerNode.jsx
+++ client/src/components/Resizer/CustomResizerNode.jsx
@@ -3,58 +3,59 @@ import { Handle, Position, NodeResizeControl } from "@xyflow/react";
 import PdftronViewer from "../Pdftron/PdftronViewer";
 
 const controlStyle = {
-    background: "transparent",
-    border: "none",
+  background: "transparent",
+  border: "none",
 };
 
 const CustomNode = ({ data }) => {
-    const renderViwer = (type) => {
-        switch (type) {
-            case "PdftronViewer":
-                return <PdftronViewer />;
-            case "PotreeViewer":
-                return <PotreeViewer />;
-            case "IfcViewer":
-                return <IfcViewer />;
-            default:
-                return null;
-        }
-    };
-    return (
-        <>
-            <NodeResizeControl style={controlStyle} minWidth={100} minHeight={50}>
-                <ResizeIcon />
-            </NodeResizeControl>
+  const renderViwer = (type) => {
+    switch (type) {
+      case "PdftronViewer":
+        return <PdftronViewer />;
+      case "PotreeViewer":
+        return <PotreeViewer />;
+      case "IfcViewer":
+        return <IfcViewer />;
+      default:
+        return null;
+    }
+  };
+  return (
+    <>
+      <NodeResizeControl style={controlStyle} minWidth={100} minHeight={50}>
+        <ResizeIcon />
+      </NodeResizeControl>
 
-            <Handle type="target" position={Position.Left} />
-            <div>{data.label}</div>
-            {renderViwer(data.viewer)}
-            <Handle type="source" position={Position.Right} />
-        </>
-    );
+      <Handle type="target" position={Position.Left} />
+      {renderViwer(data.viewer)}
+      <Handle type="source" position={Position.Right} />
+    </>
+  );
 };
 
 function ResizeIcon() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="#ff0071"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            style={{ position: "absolute", right: 5, bottom: 5 }}
-        >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <polyline points="16 20 20 20 20 16" />
-            <line x1="14" y1="14" x2="20" y2="20" />
-            <polyline points="8 4 4 4 4 8" />
-            <line x1="4" y1="4" x2="10" y2="10" />
-        </svg>
-    );
+  return (
+    <div style={{height : '20px'}}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="#ff0071"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{ position: "absolute", right: 5, bottom: 5 }}
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <polyline points="16 20 20 20 20 16" />
+        <line x1="14" y1="14" x2="20" y2="20" />
+        <polyline points="8 4 4 4 4 8" />
+        <line x1="4" y1="4" x2="10" y2="10" />
+      </svg>
+    </div>
+  );
 }
 
 export default memo(CustomNode);
